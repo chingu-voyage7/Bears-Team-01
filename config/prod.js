db2d0f31bfbea3b8357c9b54569567f425179c2a
Saung Park <spark89@gmail.com>
2018-11-19 17:17:05 -0500

    added config folder and prod.js

---
+++ config/prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    redirectDomain: process.env.REDIRECT_DOMAIN
+  };
