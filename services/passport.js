5bf5172a54c414313ae54a28c8997c1711979b6e
Saung Park <spark89@gmail.com>
2018-11-19 17:26:43 -0500

    added passport.js

---
+++ services/passport.js
@@ -0,0 +1,42 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const { googleClientID, googleClientSecret } = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: googleClientID,
+      clientSecret: googleClientSecret,
+      callbackURL: '/auth/google/callback',
+      proxy: true
+    },
+
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const existingUser = await User.findOne({ profileID: profile.id });
+
+        if (existingUser) {
+          return done(null, existingUser);
+        }
+
+        const user = await new User({ profileID: profile.id }).save();
+        done(null, user);
+      } catch (e) {
+        done(e);
+      }
+    }
+  )
+);
