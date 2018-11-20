ebff31cf3688d1ead237871863b57c0e50c48370
Crystal <hello@crystalbohacek.com>
2018-11-19 23:39:21 -0500

    changed the styling of welcome page

--- client/src/components/layout/Welcome.js
+++ client/src/components/layout/Welcome.js
@@ -8,10 +8,8 @@ class Welcome extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-12 text-center">
-              <h1 className="display-3 mb-4">BeerCraft</h1>
+              <h1 className="display-4">BeerCraft</h1>
               <p className="lead">AI-powered Beer Recommendations </p>
-              <Link to="/register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
-              <Link to="/login" className="btn btn-lg btn-light">Login</Link>
             </div>
           </div>
         </div>
