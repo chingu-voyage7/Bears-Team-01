c63f95f3aefa565e7eacf104770e111bd4e0ed70
Crystal <hello@crystalbohacek.com>
2018-11-19 23:56:13 -0500

    changed footer and index page styles

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="bg-dark text-light text-center">
+  <footer className="bg-dark text-center">
     <p>Copyright &copy; {new Date().getFullYear()} BeerCraft</p>
   </footer>
 )
