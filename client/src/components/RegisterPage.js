5a9d17309ee25b73f231f61db6e9a31f26cec966
Crystal <hello@crystalbohacek.com>
2018-11-20 10:45:35 -0500

    added register form

--- client/src/components/RegisterPage.js
+++ client/src/components/RegisterPage.js
@@ -4,7 +4,30 @@ class Register extends Component {
   render() {
     return (
       <div>
-        <h1>Register</h1>
+        <div className="aligner">
+          <div className="row aligner-item">
+            <div className="col-md-6">
+              <h1>Register</h1>
+              <div className="form-group">
+                <label for="username">Username</label>
+                <input type="text" className="form-control" name="username" placeholder="username" />
+              </div>
+              <div className="form-group">
+                <label for="username">Email</label>
+                <input type="text" className="form-control" name="email" placeholder="email" />
+              </div>
+              <div className="form-group">
+                <label for="password">Password</label>
+                <input type="password" className="form-control" name="password" placeholder="password" />
+              </div>
+              <div className="form-group">
+                <label for="username">Confirm Password</label>
+                <input type="password" className="form-control" name="confirm_password" placeholder="confirm password" />
+              </div>
+              <input type="submit" className="btn btn-primary btn-block mt-4" />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
