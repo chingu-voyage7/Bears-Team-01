ad2447c3f6dd2dbc506907db540bb76a1fcfafa7
Crystal <hello@crystalbohacek.com>
2018-11-20 00:18:39 -0500

    moved index page

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom' 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
-import Welcome from './components/layout/Welcome';
+import Welcome from './components/WelcomePage';
+import Login from './components/LoginPage';
 
 import './App.css';
 
@@ -15,6 +16,7 @@ class App extends Component {
             <div className="container">
               <Switch>
                <Route path="/" component={Welcome} exact={true} />
+               <Route path="/login" component={Login} exact={true} />
               </Switch>
              </div>
           <Footer />
