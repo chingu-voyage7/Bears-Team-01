c2b6b34adc3d9ac68ff15cc24642ae6d0ca95674
Crystal <hello@crystalbohacek.com>
2018-11-19 23:44:35 -0500

    changed the footer text color

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="bg-dark text-white text-center">
+  <footer className="bg-dark text-light text-center">
     <p>Copyright &copy; {new Date().getFullYear()} BeerCraft</p>
   </footer>
 )
