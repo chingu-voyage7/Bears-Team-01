a7cbe429e3a1dd521f98eef8247718c0ce5f3cba
Crystal <hello@crystalbohacek.com>
2018-11-19 23:35:58 -0500

    added welcome page component to index page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom' 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Welcome from './components/layout/Welcome';
+
 import './App.css';
 
 class App extends Component {
@@ -10,6 +12,11 @@ class App extends Component {
       <Router>
         <div className="App">
           <Navbar />
+            <div className="container">
+              <Switch>
+               <Route path="/" component={Welcome} exact={true} />
+              </Switch>
+             </div>
           <Footer />
         </div>
       </Router>
