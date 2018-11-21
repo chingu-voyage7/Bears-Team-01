4982e4a964d8ff62eaa36f746166b958907f6631
Crystal <hello@crystalbohacek.com>
2018-11-20 22:28:13 -0500

    imported register modal in the navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import RegisterModal from './RegisterModal';
 
 class Navbar extends Component {
   render() {
@@ -31,6 +32,7 @@ class Navbar extends Component {
             </div>
           </div>
         </nav>
+        <RegisterModal />
       </div>
     )
   }
