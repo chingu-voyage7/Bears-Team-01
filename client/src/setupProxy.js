50e31797323693d1c3d0962efa6f5f37176e1ecb
Saung Park <spark89@gmail.com>
2018-11-25 12:36:28 -0500

    added /beers route to client side proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,4 +2,5 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = app => {
   app.use(proxy('/auth/google', { target: 'http://localhost:5000' }));
+  app.use(proxy('/beers', { target: 'http://localhost:5000' }));
 };
