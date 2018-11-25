bbde383da659aa3a37ee85fc26fe159cfc38e6bc
Saung Park <spark89@gmail.com>
2018-11-25 18:09:39 -0500

    added rate categories to constants.js and created BeerReview component

---
+++ client/src/components/BeerReview.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import { RATE_CATEGORIES } from '../constants';
+// import RateCategory from './RateCategory';
+
+class BeerReview extends Component {}
--- client/src/constants.js
+++ client/src/constants.js
@@ -1,1 +1,3 @@
 export const GOOGLE_AUTH_ROUTE = '/auth/google';
+
+export const RATE_CATEGORIES = ['Look', 'Smell', 'Taste', 'Feel', 'Overall'];
