c3e9befb9c290f02956c14d00f51177c1a0c91d1
Saung Park <spark89@gmail.com>
2018-11-25 18:44:17 -0500

    created RateCategory component

---
+++ client/src/components/RateCategory.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const RateCategory = ({ categoryName, handleSelectChange }) => {
+  const firstLetterLowerCased =
+    categoryName[0].toLowerCase() + categoryName.slice(1);
+  return (
+    <div>
+      <label>
+        {categoryName}
+        <select name={firstLetterLowerCased}>{makeOptions(1, 5, 0.5)}</select>
+      </label>
+    </div>
+  );
+};
+
+const makeOptions = (start, end, step) => {
+  const optionsArr = [];
+
+  for (let i = start; i > end; i += step) {
+    optionsArr.push(<option value={i}>{i}</option>);
+  }
+
+  return optionsArr;
+};
+
+// too verbose and not easily readable
+// const makeOptions = (start, end, step, optionsArr) => {
+//   return start >= end
+//     ? optionsArr
+//     : makeOptions(start + step, end, step, [
+//         ...optionsArr,
+//         <option value={start}>{start}</option>
+//       ]);
+// };
+
+export default RateCategory;
