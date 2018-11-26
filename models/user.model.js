62d4d730f66f68b49ee70fa7a4090dd5261b5f08
Crystal <hello@crystalbohacek.com>
2018-11-26 12:41:29 -0500

    updated user model

--- models/user.model.js
+++ models/user.model.js
@@ -12,6 +12,12 @@ const UserSchema = mongoose.Schema({
     smell: Array,
     taste: Array,
     feel: Array
+  },
+  facebook: {
+    id: String,
+    token: String,
+    email: String,
+    name: String
   }
 })
 
