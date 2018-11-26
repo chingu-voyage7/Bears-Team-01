f5c218b2942d5d8094ae792caa1cda8065c495be
Crystal <hello@crystalbohacek.com>
2018-11-26 13:04:20 -0500

    added facebook strategy

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const mongoose = require('mongoose');
 const { googleClientID, googleClientSecret } = require('../config/keys');
+const configAuth = require('../do_not_commit/dev');
 
 const User = require('../models/user.model');
 
@@ -39,6 +41,42 @@ passport.use(
   )
 );
 
+
+passport.use(
+  new FacebookStrategy({
+    clientID: configAuth.facebookAuth.clientID,
+    clientSecret: configAuth.facebookAuth.clientSecret,
+    callbackURL: configAuth.facebookAuth.callbackURL,
+    profileFields: ['id', 'name', 'picture.type(large)', 'emails', 'displayName']
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function(){
+      User.findOne({ 'facebook.id' : profile.id}, function(err, user){
+        if(err)
+          return done(err);
+        if(user)
+          return done(null, user);
+        else {
+          const newUser = new User();
+          newUser.facebook.id = profile.id;
+          newUser.facebook.token = accessToken;
+          newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
+          newUser.facebook.email = profile.emails[0].value;
+
+          newUser.save(function(err){
+            if(err)
+              throw err;
+            else 
+              return done(null, newUser);
+          })
+        }
+      })
+    })
+  }
+));
+
+
+
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
@@ -52,3 +90,4 @@ passport.deserializeUser(async (id, done) => {
     return done(e, null);
   }
 });
+
