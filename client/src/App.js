7d3d556c23bb5c7c6a16ab7430413c05dcbc1fc5
Saung Park <spark89@gmail.com>
2018-11-27 21:35:06 -0500

    adjusted height, width, and centering style of container

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,11 @@ import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Welcome from './components/WelcomePage';
 import Browse from './components/BrowsePage';
-// import BeerReview from './components/BeerReview';
+import ReviewBeer from './components/ReviewBeer';
 
 import './App.css';
+
+const TEST_BEER_NAME = 'Sam Adams';
 
 class App extends Component {
   render() {
@@ -14,7 +16,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <Navbar />
-          {/* <BeerReview beerName={'HEINEKEN'}/> */}
+          <ReviewBeer beerName={TEST_BEER_NAME}/>
             <div className="container">
               <Switch>
                <Route path="/" component={Welcome} exact={true} />
