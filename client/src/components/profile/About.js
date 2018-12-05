4f2eb71ab04dd62f93a7880dd6b8010dc278f891
Crystal <hello@crystalbohacek.com>
2018-12-05 13:24:43 -0500

    added default icons

--- client/src/components/profile/About.js
+++ client/src/components/profile/About.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
-const AboutSection = () => (
-  <div>
-    This is from my about section component
-  </div>
-);
+class AboutSection extends Component {
+  render(){
+    return (
+      <div>
+      This is from my about section component
+      </div>
+    )
+  }
+}
 
 export default AboutSection;
