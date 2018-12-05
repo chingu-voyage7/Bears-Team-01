6411fb41120ecce6fa0c63772e30205683c5e001
Crystal <hello@crystalbohacek.com>
2018-12-04 22:48:16 -0500

    added profile page

---
+++ client/src/components/ProfilePage.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class ProfilePage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userData = {
+        name: ''
+      }
+    }
+  }
+  getUser = () => {
+    fetch('/user')
+    .then(response => response.json())
+    .then(userData => this.setState({userData: userData}))
+    .catch(err => console.log(err))
+  }
+  componentDidMount = () => {
+    this.getUser();
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.userData.name}</h1>
+      </div>
+    )
+  }
+}
