294c1fee8f8615c8aa3089b7f7e4e29f8093bd74
Crystal <hello@crystalbohacek.com>
2018-12-07 13:51:21 -0500

    back button styling

--- client/src/components/NotFoundPage.js
+++ client/src/components/NotFoundPage.js
@@ -6,9 +6,9 @@ const NotFoundPage = (props) => {
     <div className="text-center vertical-center">
       <h1 className="display-3 font-weight-bold">404</h1>
       <h2 className="display-5 font-weight-light">Page Not Found</h2>
-      <Link to="/">Go Home</Link>
-      {props.history.length > 0 &&
-        <button onClick={props.history.goBack}>Go Back</button>}
+      <Link className="btn btn-link go-back-btn" to="/">Go Home</Link>
+      {props.history.length > 2 &&
+        <button className="btn btn-link go-back-btn" onClick={props.history.goBack}>Go Back</button>}
         
     </div>  
   );
