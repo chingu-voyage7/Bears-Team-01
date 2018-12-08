6f418d06462c986b5dfba70d1b21d547f39c7204
Erik Herbranson <erikherbranson@tuta.io>
2018-12-08 16:41:12 -0500

    protected user routes

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user.model");
+const isLogedIn = require("../middlewares/requireLogin");
 
-router.get("/", (req, res, next) => {
-  res.status(200).json(req.user);  
+router.get("/", isLogedIn, (req, res, next) => {
+  res.status(200).json(req.user);
 });
 
 module.exports = router;
