6c0b6acf468dab0c69c7a7252556e2e45295090d
Crystal <hello@crystalbohacek.com>
2018-12-11 23:44:53 -0500

    added content boxes

--- client/src/components/BeerPage.js
+++ client/src/components/BeerPage.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class BeerPage extends Component{
+//Id: this.props.match.params.id
   render(){
     return (
     <div>
       <div className="container">
-        <p>Beer with an id of {this.props.match.params.id}</p>
-      </div>  
+        <div className="row">
+          <div className="beer-container info col-md-8">
+            <img className="main-image" src="https://i.imgur.com/oLXSUJP.png"></img>
+            <div className="name">
+              <h2>Hazy Little Thing</h2>
+              <p className="brewery">Sierra Nevada Brewing Co.</p>
+            </div>
+          </div>  
+          <div className="beer-container similar col-md-3 offset-1">
+            <h4>Similar Beers</h4>
+        </div> 
+        </div>
+      </div>
     </div>
     )
   }
