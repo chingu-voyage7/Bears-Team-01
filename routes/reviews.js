4a947b9ad9bc1780c7125ea03a03c0aea5890a5d
Crystal <hello@crystalbohacek.com>
2018-12-26 13:14:20 -0500

    review create and get route

---
+++ routes/reviews.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+const Beer = require("../models/beer.model.js");
+const Review = require("../models/review.model.js");
+const isLogedIn = require("../middlewares/requireLogin");
+
+//Reviews New
+
+router.get("/new", isLoggedIn, function(req, res){
+  //find beer by id
+  Beer.findById(req.params.id, function(err, beer){
+      if(err){
+          console.log(err);
+      } else {
+          res.redirect('/beer/' + req.params.id);
+      }
+  });
+});
+
+//Reviews Create
+
+router.post("/", isLoggedIn, function(req, res){
+  //lookup beer using ID
+  Beer.findById(req.params.id, function(err, beer){
+      if(err){
+         req.flash("error", "Error: Something went wrong!");
+         res.redirect("/browse");
+      } else {
+          Review.create(req.body.review, function(err, review){
+              if(err){
+                  console.log(err);
+              } else {
+                  review.author.id = req.user._id;
+                  review.author.username = req.user.username;
+                  review.author.name = req.user.name;
+                  review.author.picture = req.user.picture;
+                  review.date = new Date();
+                  console.log(review)
+                  //save review
+                  review.save();
+                  beer.reviews.push(review);
+                  beer.save();
+                  res.redirect("/beer/" + beer._id);
+              }
+          });
+      }
+  });
+});
