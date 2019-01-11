cb2b191f46844d23852df44966a154516c114b10
Crystal <hello@crystalbohacek.com>
2019-01-11 12:38:14 -0500

    set state functions

--- client/src/components/AddNewBeerPage.js
+++ client/src/components/AddNewBeerPage.js
@@ -12,20 +12,36 @@ class AddNewBeerPage extends Component {
       description: '',
       IBU: '',
       ABV: '',
-      style: ''
+      style: '',
+      status: ''
     }
+  }
+  handleNameChange = (e) => {
+    this.setState({ beername: e.target.value });
+  };
+  handleBrewerChange = (e) => {
+    let brewer = {...this.state.brewery}
+    brewer.name = e.target.value;
+    this.setState({brewer});
+  }
+  handleDescriptionChange = (e) => {
+    this.setState({ description: e.target.value });
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
   }
   render(){
     return (
       <div>
         <div className="container">
-          <form onSubmit={this.handleSubmitClick}>
+          <form onSubmit={this.onSubmit}>
             <div className="form-group mt-3 p-1">
               <label htmlFor="beername">
                 Name
               </label>
               <input 
-                onChange={this.handleTextAreaChange} 
+                onChange={this.handleNameChange} 
                 id="beername" 
                 className="form-control mt-1" 
                 type="text" 
@@ -38,7 +54,7 @@ class AddNewBeerPage extends Component {
                 Brewer
               </label>
               <input 
-                onChange={this.handleTextAreaChange} 
+                onChange={this.handleBrewerChange} 
                 id="brewer" 
                 className="form-control mt-1" 
                 type="text" 
@@ -64,7 +80,7 @@ class AddNewBeerPage extends Component {
                 Description
               </label>
               <textarea 
-                onChange={this.handleTextAreaChange} 
+                onChange={this.handleDescriptionChange} 
                 id="description" 
                 className="form-control mt-1" 
                 type="text" 
@@ -80,6 +96,7 @@ class AddNewBeerPage extends Component {
               />
             </div>
           </form>
+          {this.state.status.length > 0 && <p className="ml-1">{this.state.status}</p>}
         </div>
       </div>
     )
